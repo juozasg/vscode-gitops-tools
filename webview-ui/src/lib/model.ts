cc9f50267194850a1fe17233b5a807df046c3f54
Juozas G <juozasgaigalas@gmail.com>
2023-01-28 15:08:20 +0200

    helm release WIP

--- webview-ui/src/lib/model.ts
+++ webview-ui/src/lib/model.ts
@@ -7,7 +7,7 @@ import { params, ParamsDictionary } from './params';
 export const [createSource, setCreateSource] = createSignal(true);
 export const [selectedSource, setSelectedSource] = createSignal('');
 
-export const [commonResource, setCommonResource] = createStore({
+export const [getSource, setSource] = createStore({
 	kind: 'GitRepository',
 
 	name: 'podinfo',
@@ -17,7 +17,7 @@ export const [commonResource, setCommonResource] = createStore({
 	timeout: '5m0s',
 });
 
-export const [gitSource, setGitSource] = createStore({
+export const [getGitRepository, setGitRepository] = createStore({
 	url: 'https://github.com/stefanprodan/podinfo',
 	refType: 'branch',
 	ref: 'master',
@@ -40,7 +40,7 @@ export const [gitSource, setGitSource] = createStore({
 	ignorePaths: '',
 });
 
-export const [helmSource, setHelmSource] = createStore({
+export const [getHelmRepository, setHelmRepository] = createStore({
 	url: 'https://stefanprodan.github.io/podinfo',
 
 	// sync
@@ -57,7 +57,7 @@ export const [helmSource, setHelmSource] = createStore({
 	passCredentials: false,
 });
 
-export const [ociSource, setOCISource] = createStore({
+export const [getOCIRepository, setOCIRepository] = createStore({
 	url: 'oci://ghcr.io/stefanprodan/manifests/podinfo',
 	ref: 'latest',
 	refType: 'tag',
@@ -70,7 +70,7 @@ export const [ociSource, setOCISource] = createStore({
 	serviceAccount: '',
 });
 
-export const [bucketSource, setBucketSource] = createStore({
+export const [getBucket, setBucket] = createStore({
 	// bucketEndpoint: 'minio.minio.svc.cluster.local:9000',
 	endpoint: '',
 	buckerName: 'podinfo',
@@ -90,7 +90,7 @@ export const [bucketSource, setBucketSource] = createStore({
 
 export const [createWorkload, setCreateWorkload] = createSignal(false);
 
-export const [kustomization, setKustomization] = createStore({
+export const [getKustomization, setKustomization] = createStore({
 	name: 'podinfo',
 	namespace: 'flux-system',
 	source: '',  // Ex: GitRepo/podinfo.flux-system
@@ -101,35 +101,57 @@ export const [kustomization, setKustomization] = createStore({
 	prune: true,
 });
 
-export const [helmRelease, setHelmReleast] = createStore({
+export const [getHelmRelease, setHelmRelease] = createStore({
 
-	--chart string                   Helm chart name or path
-	--chart-interval duration        the interval of which to check for new chart versions
-	--chart-version string           Helm chart version, accepts a semver range (ignored for charts from GitRepository sources)
-	--crds crds                      upgrade CRDs policy, available options are: (Skip, Create, CreateReplace)
-	--create-target-namespace        create the target namespace if it does not exist
-	--depends-on strings             HelmReleases that must be ready before this release can be installed, supported formats '<name>' and '<namespace>/<name>'
--h, --help                           help for helmrelease
-	--kubeconfig-secret-ref string   the name of the Kubernetes Secret that contains a key with the kubeconfig file for connecting to a remote cluster
-	--reconcile-strategy string      the reconcile strategy for helm chart created by the helm release(accepted values: Revision and ChartRevision) (default "ChartVersion")
-	--release-name string            name used for the Helm release, defaults to a composition of '[<target-namespace>-]<HelmRelease-name>'
-	--service-account string         the name of the service account to impersonate when reconciling this HelmRelease
-	--source helmChartSource         source that contains the chart in the format '<kind>/<name>.<namespace>', where kind must be one of: (HelmRepository, GitRepository, Bucket)
-	--target-namespace string        namespace to install this release, defaults to the HelmRelease namespace
-	--values strings                 local path to values.yaml files, also accepts comma-separated values
-	--values-from strings            a Kubernetes object reference that contains the values.yaml data key in the format '<kind>/<name>', where kind must be one of: (Secret,ConfigMap)
+	// 	--chart string                   Helm chart name or path
+	// 	--chart-interval duration        the interval of which to check for new chart versions
+	// 	--chart-version string           Helm chart version, accepts a semver range (ignored for charts from GitRepository sources)
+	// 	--crds crds                      upgrade CRDs policy, available options are: (Skip, Create, CreateReplace)
+	// 	--create-target-namespace        create the target namespace if it does not exist
+	// 	--depends-on strings             HelmReleases that must be ready before this release can be installed, supported formats '<name>' and '<namespace>/<name>'
+	// -h, --help                           help for helmrelease
+	// 	--kubeconfig-secret-ref string   the name of the Kubernetes Secret that contains a key with the kubeconfig file for connecting to a remote cluster
+	// 	--reconcile-strategy string      the reconcile strategy for helm chart created by the helm release(accepted values: Revision and ChartRevision) (default "ChartVersion")
+	// 	--release-name string            name used for the Helm release, defaults to a composition of '[<target-namespace>-]<HelmRelease-name>'
+	// 	--service-account string         the name of the service account to impersonate when reconciling this HelmRelease
+	// 	--source helmChartSource         source that contains the chart in the format '<kind>/<name>.<namespace>', where kind must be one of: (HelmRepository, GitRepository, Bucket)
+	// 	--target-namespace string        namespace to install this release, defaults to the HelmRelease namespace
+	// 	--values strings                 local path to values.yaml files, also accepts comma-separated values
+	// 	--values-from strings            a Kubernetes object reference that contains the values.yaml data key in the format '<kind>/<name>', where kind must be one of: (Secret,ConfigMap)
 
 
 });
-export function getStore(storeName: string, variable: string) {
-	const store = (storeName === 'source') ? source : kustomization;
-	return () => store[variable];
+// export function getStore(storeName: string, variable: string) {
+// 	const store = (storeName === 'source') ? source : kustomization;
+// 	return () => store[variable];
+// }
+
+// export function setStore(storeName: string, variable: any) {
+// 	const setter = (storeName === 'source') ? setSource : setKustomization;
+// 	return (val: any) => setter(variable, val);
+// }
+
+interface StoreMap {
+	[key: string]: any;
 }
 
-export function setStore(storeName: string, variable: any) {
-	const setter = (storeName === 'source') ? setSource : setKustomization;
-	return (val: any) => setter(variable, val);
-}
+const getters: StoreMap = {
+	getSource,
+	getGitRepository,
+	getOCIRepository,
+	getHelmRepository,
+	getKustomization,
+	getHelmRelease,
+};
+
+const setters: StoreMap = {
+	setSource,
+	setGitRepository,
+	setOCIRepository,
+	setHelmRepository,
+	setKustomization,
+	setHelmRelease,
+};
 
 export function storeAccessors(props: any) {
 	let get: ()=> any;
@@ -139,8 +161,14 @@ export function storeAccessors(props: any) {
 	set = props.set || ((_: any) => undefined);
 
 	if(props.store && props.field) {
-		get = getStore(props.store, props.field);
-		set = setStore(props.store, props.field);
+		const store = props.store;
+		const field = props.field;
+
+		const getter = getters[`get${store}`];
+		const setter = getters[`set${store}`];
+
+		get = () => getter[field];
+		set = val => setter(field, val);
 	}
 
 	return {get, set};
