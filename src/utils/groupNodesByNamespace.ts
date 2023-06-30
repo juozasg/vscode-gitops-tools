9f070a812d365f0cbbef4120101f830e15d376eb
Juozas Gaigalas <juozasgaigalas@gmail.com>
2023-06-30 15:46:53 +0300

    'rename and extract groupNodesByNamespace'

---
+++ src/utils/groupNodesByNamespace.ts
@@ -0,0 +1,24 @@
+import { Namespace } from 'types/kubernetes/kubernetesTypes';
+import { NamespaceNode } from '../ui/treeviews/nodes/namespaceNode';
+import { TreeNode } from '../ui/treeviews/nodes/treeNode';
+import { getNamespaces } from 'cli/kubernetes/kubectlGet';
+
+
+export async function groupNodesByNamespace(nodes: TreeNode[]): Promise<NamespaceNode[]> {
+	const namespaceNodes: NamespaceNode[] = [];
+
+	const namespaces: Namespace[] = await getNamespaces();
+
+	namespaces.forEach(ns => {
+		const name = ns.metadata.name;
+
+		const nsChildNodes = nodes.filter(node => node.resource?.metadata?.namespace === name);
+		if (nsChildNodes.length > 0) {
+			const nsNode = new NamespaceNode(ns);
+			nsChildNodes.forEach(childNode => nsNode.addChild(childNode));
+			namespaceNodes.push(nsNode);
+		}
+	});
+
+	return namespaceNodes;
+}
